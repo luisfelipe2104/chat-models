842403a80f0f72398b99a329368c6b809d74b8a5
unknown <gomesluisfelipe05@gmail.com>
2022-10-13 15:16:57 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import { ChatModel } from "./chat.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json()) 
+const PORT = process.env.PORT || 5555
+
+const mongoURL = "mongodb+srv://luis:felipe05@chat.g7ebxn3.mongodb.net/Chat?retryWrites=true&w=majority"
+
+// connects to the mongo db
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+})
+
+app.post("/insert", async (req, res) => {
+    const message = req.body.message
+    const author = req.body.author
+    const time = req.body.time
+    const room = req.body.room
+
+    const chat = new ChatModel({
+        message: message,
+        author: author,
+        time: time,
+        room: room,
+    })
+
+    try{
+        await chat.save()
+        console.log("saved!")
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+app.get("/get-message/:room", async (req, res) => {
+    const room = req.params.room
+
+    console.log(room)
+    ChatModel.find({room: room}, (err, result) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(result)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("listening on port: " + PORT)
+}) 
